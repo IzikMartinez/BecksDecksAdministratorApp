750b008058cc4553bbf35c36c69a93339570f506
Isaac Martinez <IsaacR454@gmail.com>
2024-04-29 08:33:41 -0500

    image upload in product creation page widget is complete

--- lib/add_product.dart
+++ lib/add_product.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'constants.dart';
@@ -18,14 +21,22 @@ class _AddProductDBState extends State<AddProductDB> {
   final _stockController = TextEditingController();
   final _categoryController = TextEditingController();
 
+  File? _selectedImage;
   Future<void> insertProduct() async {
-    await supabase
-        .from('PRODUCTS')
-        .insert({
-          'name': _nameController.text,
-          'desc': _descController.text,
-          'price': _priceController.text,
-          'category': _categoryController.text
+    await supabase.from('PRODUCTS').insert({
+      'name': _nameController.text,
+      'desc': _descController.text,
+      'price': _priceController.text,
+      'category': _categoryController.text
+    });
+  }
+
+  Future _pickImageFromGallery() async {
+    final productImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (productImage == null) return;
+    setState(() {
+      _selectedImage = File(productImage.path);
     });
   }
 
@@ -34,110 +45,150 @@ class _AddProductDBState extends State<AddProductDB> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
           title: const Text('Add Product to Database'),
           backgroundColor: Colors.deepPurpleAccent,
-      ),
-      body: _loading
-          ? const Center(child: CircularProgressIndicator(),)
-          : Padding(
-        padding: const EdgeInsets.symmetric(vertical: 28, horizontal: 12),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              SizedBox(
-                width: 500,
-                height: 100,
-                child:
-              TextFormField(
-                controller: _nameController,
-                decoration: const InputDecoration(labelText: 'Product Name'),
-                validator: (value) {
-                  if(value == null || value.isEmpty) {
-                    return "Enter the name of the product";
-                  }
-                  return null;
-                },
-              ),),
-              SizedBox(
-                width: 500,
-                height: 100,
-                child: TextFormField(
-                controller: _descController,
-                decoration: const InputDecoration(labelText: "Product Description"),
-                validator: (value) {
-                  if(value == null || value.isEmpty) {
-                    return "Enter a description for the product";
-                  }
-                  return null;
-                },
-              ),
-              ),
-              SizedBox(
-                width: 500,
-                height: 100,
-                child: TextFormField(
-                  controller: _priceController,
-                  decoration: const InputDecoration(labelText: "Product Price"),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if(value == null || value.isEmpty) {
-                      return "Enter the price for the product (numbers only, no dollar signs)";
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              SizedBox(
-                width: 500,
-                height: 100,
-                child: TextFormField(
-                  controller: _stockController,
-                  decoration: const InputDecoration(labelText: "OPTIONAL: Quantity in-stock"),
-                  keyboardType: TextInputType.number,
-                ),
-              ),
-              DropdownMenu(
-                onSelected: (String? value) {
-                  if(value != null) {
-                    setState(() {
-                      _categoryController.text = value;
-                    });
-                  }
-                },
-                label: const Text("Category"),
-                helperText: "Select what category a product belongs to. Product will be autosorted on the app",
-                width: 500,
-                dropdownMenuEntries: const <DropdownMenuEntry<String>>[
-                  DropdownMenuEntry(value: "magic", label: "Magic"),
-                  DropdownMenuEntry(value: "fab", label: "Flesh & Blood"),
-                  DropdownMenuEntry(value: "yugioh", label: "Yu-Gi-Oh"),
-                  DropdownMenuEntry(value: "lorcana", label: "Lorcana"),
-                  DropdownMenuEntry(value: "pokemon", label: "Pokemon"),
-                ],
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: Text('''
+        ),
+        body: _loading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 28, horizontal: 12),
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: 350,
+                          height: 100,
+                          child: TextFormField(
+                            controller: _nameController,
+                            decoration: const InputDecoration(
+                                labelText: 'Product Name'),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Enter the name of the product";
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          width: 350,
+                          height: 100,
+                          child: TextFormField(
+                            controller: _descController,
+                            decoration: const InputDecoration(
+                                labelText: "Product Description"),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Enter a description for the product";
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          width: 350,
+                          height: 100,
+                          child: TextFormField(
+                            controller: _priceController,
+                            decoration: const InputDecoration(
+                                labelText: "Product Price"),
+                            keyboardType: TextInputType.number,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return "Enter the price for the product (numbers only, no dollar signs)";
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          width: 350,
+                          height: 100,
+                          child: TextFormField(
+                            controller: _stockController,
+                            decoration: const InputDecoration(
+                                labelText: "OPTIONAL: Quantity in-stock"),
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                        DropdownMenu(
+                          onSelected: (String? value) {
+                            if (value != null) {
+                              setState(() {
+                                _categoryController.text = value;
+                              });
+                            }
+                          },
+                          label: const Text("Category"),
+                          helperText:
+                              "Select what category a product belongs to. Product will be autosorted on the app",
+                          width: 350,
+                          dropdownMenuEntries: const <DropdownMenuEntry<
+                              String>>[
+                            DropdownMenuEntry(value: "magic", label: "Magic"),
+                            DropdownMenuEntry(
+                                value: "fab", label: "Flesh & Blood"),
+                            DropdownMenuEntry(
+                                value: "yugioh", label: "Yu-Gi-Oh"),
+                            DropdownMenuEntry(
+                                value: "lorcana", label: "Lorcana"),
+                            DropdownMenuEntry(
+                                value: "pokemon", label: "Pokemon"),
+                          ],
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              _pickImageFromGallery();
+                            },
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.blue,
+                                foregroundColor: Colors.white,
+                                padding: const EdgeInsets.all(8.0)),
+                            child: const Text(
+                              "Upload product image",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            )),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        _selectedImage != null
+                            ? Image.file(_selectedImage!)
+                            : const Text("Please select an image"),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(SnackBar(content: Text('''
                           Created new product with following properties:
                           Name: ${_nameController.text}
                           Desc: ${_descController.text}
                           Price: ${_priceController.text}
                           Category: ${_categoryController.text}
                     ''')));
-                    insertProduct();
-                  }
-                },
-                child: const Text("Validate"),
-              )
-          ],
-      )
-      )
-      )
-    );
+                              insertProduct();
+                              setState(() {
+                                _nameController.text = '';
+                                _descController.text = '';
+                                _priceController.text = '';
+                                _categoryController.text = '';
+                              });
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.amber,
+                          ),
+                          child: const Text("Submit Product"),
+                        )
+                      ],
+                    ))));
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -23,14 +23,42 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
+
 class _HomePageState extends State<HomePage> {
-  final _future = supabase.from('PRODUCTS').select();
+  final productStream = supabase.from('PRODUCTS').stream(primaryKey: ['id']);
+
+
+  Future<void> updateName (String productId, String updatedNote) async {
+    await supabase
+        .from('PRODUCTS')
+        .update({'name': updatedNote}).match({'id': productId});
+  }
+  Future<void> updateDesc (String productId, String updatedNote) async {
+    await supabase
+        .from('PRODUCTS')
+        .update({'desc': updatedNote}).match({'id': productId});
+  }
+  Future<void> updatePrice(String productId, String updatedNote) async {
+    await supabase
+        .from('PRODUCTS')
+        .update({'price': updatedNote}).match({'id': productId});
+  }
+  Future<void> updateCategory(String productId, String updatedNote) async {
+    await supabase
+        .from('PRODUCTS')
+        .update({'category': updatedNote}).match({'id': productId});
+  }
+
+  Future<void> deleteProduct(String productId) async {
+    await supabase.from('PRODUCTS').delete().match({'id': productId});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,8 +66,8 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
           backgroundColor: Colors.green,
           title: const Text("Beck's Decks Admin App")),
-      body: FutureBuilder(
-        future: _future,
+      body: StreamBuilder(
+        stream: productStream,
         builder: (context, snapshot) {
           if (!snapshot.hasData) {
             return const Center(child: CircularProgressIndicator());
@@ -49,81 +77,120 @@ class _HomePageState extends State<HomePage> {
             itemCount: products.length,
             itemBuilder: ((context, index) {
               final product = products[index];
+              final productId = product['id'];
               return Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0)
-                ),
-                elevation: 5,
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                      child: FittedBox(child: Text('\$${product['price']}'),),
-                  ),
-                  title: Text("PRODUCT: ${product['name']}"),
-                  trailing: Container(
-                    width: 200,
-                    child: Row(
-                      children: <Widget>[
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.edit),
-                          color: Theme.of(context).primaryColor,
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            showDialog(context: context, builder:(BuildContext) {
-                              return AlertDialog(
-                                title: const Text('Confirm Deletion'),
-                                content: const Text('Are you sure you want to delete this product from the database? No takebackies'),
-                                actions: <Widget>[
-                                  ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                      fixedSize: const Size.fromWidth(100),
-                                      padding: const EdgeInsets.all(10),
-                                      backgroundColor: Colors.green,
-                                      foregroundColor: Colors.white,
-                                    ),
-                                    child: const Text('Cancel'),
-                                    onPressed: () {
-                                      Navigator.of(context).pop(); // close dialog
-                                    },
-                                  ),
-                                  ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                      fixedSize: const Size.fromWidth(100),
-                                      padding: const EdgeInsets.all(10),
-                                      backgroundColor: Colors.red,
-                                      foregroundColor: Colors.white,
-                                    ),
-                                    child: const Text('Yes'),
-                                    onPressed: () async {
-                                      Navigator.of(context).pop(); // close dialog
-                                      await supabase
-                                          .from('PRODUCTS')
-                                          .delete()
-                                          .match({'id': product['id']});
-                                    },
-                                  )
-                                ],
-
-                              );
-                            });
-                          },
-                          icon: const Icon(Icons.delete),
-                          color: Colors.red,
-                        )
-                      ],
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  elevation: 3,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: FittedBox(
+                        child: Text('\$${product['price']}'),
+                      ),
                     ),
-                  ),
-
-              )
-              );
+                    title: Text("${product['name']}"),
+                    trailing: Container(
+                      width: 100,
+                      height: 40,
+                      child: Row(
+                        children: <Widget>[
+                          IconButton(
+                            onPressed: () {
+                              showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return SimpleDialog(
+                                      title: const Text('Edit a product'),
+                                      children: [
+                                        TextFormField(
+                                          initialValue: product['name'],
+                                          onFieldSubmitted: (value) async {
+                                            await updateName(
+                                                productId, value);
+                                            if (mounted) Navigator.pop(context);
+                                          },
+                                        ),
+                                        TextFormField(
+                                          initialValue: product['desc'],
+                                          onFieldSubmitted: (value) async {
+                                            await updateDesc(
+                                                productId, value);
+                                            if (mounted) Navigator.pop(context);
+                                          },
+                                        ),
+                                        TextFormField(
+                                          keyboardType: TextInputType.number,
+                                          initialValue: product['price'].toString() ,
+                                          onFieldSubmitted: (value) async {
+                                            await updatePrice(
+                                                productId, value);
+                                            if (mounted) Navigator.pop(context);
+                                          },
+                                        )
+                                      ],
+                                    );
+                                  });
+                            },
+                            icon: const Icon(Icons.edit),
+                            color: Theme.of(context).primaryColor,
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              showDialog(
+                                  context: context,
+                                  builder: (BuildContext) {
+                                    return AlertDialog(
+                                      title: const Text('Confirm Deletion'),
+                                      content: const Text(
+                                          'Are you sure you want to delete this product from the database? No takebackies'),
+                                      actions: <Widget>[
+                                        ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                            fixedSize:
+                                                const Size.fromWidth(100),
+                                            padding: const EdgeInsets.all(10),
+                                            backgroundColor: Colors.green,
+                                            foregroundColor: Colors.white,
+                                          ),
+                                          child: const Text('Cancel'),
+                                          onPressed: () {
+                                            Navigator.of(context)
+                                                .pop(); // close dialog
+                                          },
+                                        ),
+                                        ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                            fixedSize:
+                                                const Size.fromWidth(100),
+                                            padding: const EdgeInsets.all(10),
+                                            backgroundColor: Colors.red,
+                                            foregroundColor: Colors.white,
+                                          ),
+                                          child: const Text('Yes'),
+                                          onPressed: () async {
+                                            deleteProduct(productId);
+                                            Navigator.of(context)
+                                                .pop(); // close dialog
+                                          },
+                                        )
+                                      ],
+                                    );
+                                  });
+                            },
+                            icon: const Icon(Icons.delete),
+                            color: Colors.red,
+                          )
+                        ],
+                      ),
+                    ),
+                  ));
             }),
           );
         },
       ),
       floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           FloatingActionButton(
             onPressed: () {
@@ -136,26 +203,6 @@ class _HomePageState extends State<HomePage> {
             },
             backgroundColor: Colors.green,
             child: const Icon(Icons.add),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              // Add your onPressed code here!
-            },
-            backgroundColor: Colors.blue,
-            child: const Icon(Icons.edit),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              // Add your onPressed code here!
-            },
-            backgroundColor: Colors.red,
-            child: const Icon(Icons.delete),
           ),
         ],
       ),
